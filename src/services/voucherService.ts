46f853a78a88ee8e0044eafad1e049b6744c9279
Jean Gabriel Frusca <jeang96@gmail.com>
2023-03-17 16:47:48 -0300

    feat: added tests to verify discount, voucher used and amount value

--- src/services/voucherService.ts
+++ src/services/voucherService.ts
@@ -1,57 +1,70 @@
 import { Voucher } from "@prisma/client";
 import voucherRepository from "../repositories/voucherRepository";
-import { conflictError } from "../utils/errorUtils";
+import { badRequestError, conflictError } from "../utils/errorUtils";
 
 const MIN_VALUE_FOR_DISCOUNT = 100;
 
 export type VoucherCreateData = Omit<Voucher, "id">;
 export interface VoucherApplyData {
-  code: string;
-  amount: number;
+	code: string;
+	amount: number;
 }
 
 async function createVoucher(code: string, discount: number) {
-  const voucher = await voucherRepository.getVoucherByCode(code);
-  if (voucher) {
-    throw conflictError("Voucher already exist.");
-  }
+	if (discount < 1 || discount > 100) {
+		throw badRequestError("Discount value is invalid.");
+	}
 
-  await voucherRepository.createVoucher(code, discount);
+	const voucher = await voucherRepository.getVoucherByCode(code);
+	if (voucher) {
+		throw conflictError("Voucher already exist.");
+	}
+
+	await voucherRepository.createVoucher(code, discount);
 }
 
 async function applyVoucher(code: string, amount: number) {
-  const voucher = await voucherRepository.getVoucherByCode(code);
-  if (!voucher) {
-    throw conflictError("Voucher does not exist.");
-  }
+	const voucher = await voucherRepository.getVoucherByCode(code);
+	if (!voucher) {
+		throw conflictError("Voucher does not exist.");
+	}
 
-  let finalAmount = amount;
-  if (isAmountValidForDiscount(amount) && !voucher.used) {
-    await changeVoucherToUsed(code);
-    finalAmount = applyDiscount(amount, voucher.discount);
-  }
+	if (!isAmountValidForDiscount(amount)) {
+		throw badRequestError("Amount value is invalid.");
+	}
 
-  return {
-    amount,
-    discount: voucher.discount,
-    finalAmount,
-    applied: finalAmount !== amount
-  }
+	let finalAmount = amount;
+
+	if (voucher.used) {
+		throw conflictError("Voucher was already used.");
+	}
+
+	if (isAmountValidForDiscount(amount) && !voucher.used) {
+		await changeVoucherToUsed(code);
+		finalAmount = applyDiscount(amount, voucher.discount);
+	}
+
+	return {
+		amount,
+		discount: voucher.discount,
+		finalAmount,
+		applied: finalAmount !== amount,
+	};
 }
 
 async function changeVoucherToUsed(code: string) {
-  return await voucherRepository.useVoucher(code);
+	return await voucherRepository.useVoucher(code);
 }
 
 function isAmountValidForDiscount(amount: number) {
-  return amount >= MIN_VALUE_FOR_DISCOUNT;
+	return amount >= MIN_VALUE_FOR_DISCOUNT;
 }
 
 function applyDiscount(value: number, discount: number) {
-  return value - (value * (discount / 100));
+	return value - value * (discount / 100);
 }
 
 export default {
-  createVoucher,
-  applyVoucher
-}
+	createVoucher,
+	applyVoucher,
+};
--- src/tests/unit/voucherService.spec.ts
+++ src/tests/unit/voucherService.spec.ts
@@ -1,7 +1,7 @@
 import voucherService from "services/voucherService";
 import { jest } from "@jest/globals";
 import voucherRepository from "repositories/voucherRepository";
-import { AppError, conflictError, isAppError } from "utils/errorUtils";
+import { badRequestError, conflictError } from "utils/errorUtils";
 
 describe("Create Voucher Test Suite", () => {
 	it("should not be able to create a voucher that already exists", () => {
@@ -24,6 +24,15 @@ describe("Create Voucher Test Suite", () => {
 
 			await voucherService.createVoucher(voucher.code, voucher.discount);
 		}).rejects.toMatchObject(conflictError("Voucher already exist."));
+	});
+
+	it("should not be able to create a voucher if discount value is above 100 or below 1", () => {
+		expect(async () => {
+			const code = "teste123";
+			const discount = 101;
+
+			await voucherService.createVoucher(code, discount);
+		}).rejects.toMatchObject(badRequestError("Discount value is invalid."));
 	});
 
 	it("should be able to create a voucher succesfully", async () => {
@@ -52,5 +61,104 @@ describe("Create Voucher Test Suite", () => {
 });
 
 describe("Apply Voucher Test Suite", () => {
-	it("should not be able to apply a voucher if it does not exists", () => {});
+	it("should not be able to apply a voucher if it does not exists", () => {
+		expect(async () => {
+			const voucherCode = "naoexisto";
+			const amount = 101;
+			jest
+				.spyOn(voucherRepository, "getVoucherByCode")
+				.mockImplementationOnce((): any => {
+					return null;
+				});
+			await voucherService.applyVoucher(voucherCode, amount);
+		}).rejects.toMatchObject(conflictError("Voucher does not exist."));
+	});
+
+	it("should not be able to apply a voucher that was already used", () => {
+		expect(async () => {
+			const voucher = {
+				code: "123FuiUsado",
+				discount: 50,
+			};
+
+			const amount = 101;
+
+			jest
+				.spyOn(voucherRepository, "getVoucherByCode")
+				.mockImplementationOnce((): any => {
+					return {
+						id: 1,
+						code: voucher.code,
+						discount: voucher.discount,
+						used: true,
+					};
+				});
+			await voucherService.applyVoucher(voucher.code, amount);
+		}).rejects.toMatchObject(conflictError("Voucher was already used."));
+	});
+
+	it("should not be able to apply a voucher if the amount is below 100", () => {
+		expect(async () => {
+			const voucher = {
+				code: "123abc",
+				discount: 50,
+			};
+
+			const amount = 99;
+
+			jest
+				.spyOn(voucherRepository, "getVoucherByCode")
+				.mockImplementationOnce((): any => {
+					return {
+						id: 1,
+						code: voucher.code,
+						discount: voucher.discount,
+						used: false,
+					};
+				});
+
+			await voucherService.applyVoucher(voucher.code, amount);
+		}).rejects.toMatchObject(badRequestError("Amount value is invalid."));
+	});
+
+	it("should be able to apply a voucher succesfully", async () => {
+		const voucher = {
+			code: "123euExisto",
+			discount: 50,
+		};
+		const amount = 120;
+
+		jest
+			.spyOn(voucherRepository, "getVoucherByCode")
+			.mockImplementationOnce((): any => {
+				return {
+					id: 1,
+					code: voucher.code,
+					discount: voucher.discount,
+					used: false,
+				};
+			});
+
+		jest
+			.spyOn(voucherRepository, "useVoucher")
+			.mockImplementationOnce((): any => {
+				return {
+					id: 1,
+					code: voucher.code,
+					discount: voucher.discount,
+					used: true,
+				};
+			});
+
+		const result = await voucherService.applyVoucher(voucher.code, amount);
+
+		expect(result).toMatchObject(
+			expect.objectContaining({
+				amount: expect.any(Number),
+				discount: voucher.discount,
+				finalAmount: expect.any(Number),
+				applied: expect.any(Boolean),
+			})
+		);
+	});
 });
